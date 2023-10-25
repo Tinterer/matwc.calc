a2db4922b29487b1de533e526cd6888a7ea68db1
Tinterer <matveyteslovv@gmail.comm>
2023-10-26 00:24:43 +0300

    new file:   pc_vers/physics/mechanic/ConsEnLaw.js 	new file:   pc_vers/physics/mechanic/KinetikEnerg.js 	new file:   pc_vers/physics/mechanic/PotencEnerg.js 	modified:   pc_vers/physics/mechanic/index.html 	modified:   pc_vers/physics/mechanic/style.css

---
+++ pc_vers/physics/mechanic/KinetikEnerg.js
@@ -0,0 +1,165 @@
+function Mass_kinetik() {
+
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass_kin').value);
+    var v = Number(document.getElementById('velocity_kinetik').value);
+    var e_k = Number(document.getElementById('e_kinetik').value);
+
+    x = m * h * e_k
+
+    if (m != 0) {
+        Solve = `Масса уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if (v != 0) {
+                m = (2 * e_k) / v ** 2 
+
+                Solve = `${m}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_kin").innerHTML = Solve;
+    document.getElementById("Solve_1_kin").innerHTML = Solve1;
+    document.getElementById("Solve_2_kin").innerHTML = Solve2;
+    document.getElementById("Solve_3_kin").innerHTML = Solve3;
+    document.getElementById("Solve_4_kin").innerHTML = Solve4;
+}
+
+function Velocity_kinetik() {
+
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass_kin').value);
+    var v = Number(document.getElementById('velocity_kinetik').value);
+    var e_k = Number(document.getElementById('e_kinetik').value);
+    i = 1/2
+
+    x = m * h * e_k
+
+    if (m != 0) {
+        Solve = `Скорость уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if ((2 * e_k) / m >= 0) {
+                v = ((2 * e_k) / m) ** i
+
+                Solve = `${v}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_kin").innerHTML = Solve;
+    document.getElementById("Solve_1_kin").innerHTML = Solve1;
+    document.getElementById("Solve_2_kin").innerHTML = Solve2;
+    document.getElementById("Solve_3_kin").innerHTML = Solve3;
+    document.getElementById("Solve_4_kin").innerHTML = Solve4;
+}
+
+function Energy_kinetik() {
+
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass_kin').value);
+    var v = Number(document.getElementById('velocity_kinetik').value);
+    var e_k = Number(document.getElementById('e_kinetik').value);
+
+    x = m * h * e_k
+
+    if (e_k != 0) {
+        Solve = `Энергия уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if (m != 0) {
+                e_k = (m * v**2)/2 
+
+                Solve = `${e_k}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_kin").innerHTML = Solve;
+    document.getElementById("Solve_1_kin").innerHTML = Solve1;
+    document.getElementById("Solve_2_kin").innerHTML = Solve2;
+    document.getElementById("Solve_3_kin").innerHTML = Solve3;
+    document.getElementById("Solve_4_kin").innerHTML = Solve4;
+}
---
+++ pc_vers/physics/mechanic/PotencEnerg.js
@@ -0,0 +1,168 @@
+function Mass_potenc() {
+
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass').value);
+    var h = Number(document.getElementById('height').value);
+    var e_p = Number(document.getElementById('e_potc').value);
+    var g = 10
+
+    x = m * h * e_p
+
+    if (m != 0) {
+        Solve = `Масса уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if (h != 0) {
+                m = e_p / (g * h)
+
+                Solve = `${m}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_ep").innerHTML = Solve;
+    document.getElementById("Solve_1_ep").innerHTML = Solve1;
+    document.getElementById("Solve_2_ep").innerHTML = Solve2;
+    document.getElementById("Solve_3_ep").innerHTML = Solve3;
+    document.getElementById("Solve_4_ep").innerHTML = Solve4;
+}
+
+function Height_potenc() {
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass').value);
+    var h = Number(document.getElementById('height').value);
+    var e_p = Number(document.getElementById('e_potc').value);
+    var g = 10;
+
+    x = m * h * e_p
+
+    if (h != 0) {
+        Solve = `Высота уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if (m != 0) {
+                h = e_p / (g * m)
+
+                Solve = `${h}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_ep").innerHTML = Solve;
+    document.getElementById("Solve_1_ep").innerHTML = Solve1;
+    document.getElementById("Solve_2_ep").innerHTML = Solve2;
+    document.getElementById("Solve_3_ep").innerHTML = Solve3;
+    document.getElementById("Solve_4_ep").innerHTML = Solve4;
+}
+
+function Energ_potenc() {
+    var Solve;
+    var Solve1;
+    var Solve2;
+    var Solve3;
+    var Solve4;
+
+    var m = Number(document.getElementById('mass').value);
+    var h = Number(document.getElementById('height').value);
+    var e_p = Number(document.getElementById('e_potc').value);
+    var g = 10
+
+    x = m * h * e_p
+
+    if (e_p != 0) {
+        Solve = `Потенциальная энергия уже известна`
+        Solve1 = ` `
+        Solve2 = ` `
+        Solve3 = ` `
+        Solve4 = ` `
+        }
+    else {
+        if (x != 0) {
+            Solve = `Все данные известны`
+            Solve1 = ` `
+            Solve2 = ` `
+            Solve3 = ` `
+            Solve4 = ` `  
+        }
+        else {
+            if (m != 0) {
+                e_p = m * g * h
+
+                Solve = `${e_p}`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `
+            } else {
+                Solve = `Проверьте правильность введенных данных`
+                Solve1 = ` `
+                Solve2 = ` `
+                Solve3 = ` `
+                Solve4 = ` `    
+            }
+        }
+    }
+
+    document.getElementById("Solve_ep").innerHTML = Solve;
+    document.getElementById("Solve_1_ep").innerHTML = Solve1;
+    document.getElementById("Solve_2_ep").innerHTML = Solve2;
+    document.getElementById("Solve_3_ep").innerHTML = Solve3;
+    document.getElementById("Solve_4_ep").innerHTML = Solve4;
+}
+
+
+
